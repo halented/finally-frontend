8802f80f325aa47673781d5e986752e38f418673
Hal Dunn <hallie.dunn1@gmail.com>
2019-12-02 14:12:06 -0800

    landing page transition animation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <div className='flipBox'>
-        FINALLY! AN APP FOR EXTROVERTS
+      <div className="flipCard">
+        <div className="flipCardInner">
+          <div className='flipBoxFront'>
+          FINALLY! AN APP FOR EXTROVERTS
+          </div>
+          <div className="flipBoxBack">
+            CLICK HERE TO GET STARTED
+          </div>
+        </div>
       </div>
     </div>
   );
