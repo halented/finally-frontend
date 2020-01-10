7f31a73ecedbba6eef2ed86255decd9556779b3f
Hal Dunn <hallie.dunn1@gmail.com>
2020-01-09 21:13:13 -0800

    fixed position of info links

--- src/components/pre-site-stuff/Infopage.js
+++ src/components/pre-site-stuff/Infopage.js
@@ -6,7 +6,12 @@ import Radium from 'radium';
 
 var thisStyle = {
     base: {
-        justifyContent: 'center'
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    block: {
+        margin: '15vh',
+        width: '80vw'
     }
 }
 
@@ -15,13 +20,13 @@ function Infopage(props){
      <div className='App' style={thisStyle.base}>
          <div>
             {props.trait === 'about' ?
-                <div>
+                <div style={thisStyle.block}>
                     <img src={icon} className='infoIcon' alt='Finally! App Icon'/>
                     <b style={{fontSize: 'larger'}}><span style={{fontWeight: 'bold'}}>FINALLY!</span> AN APP FOR EXTROVERTS </b>
                     was born out of a passion for quiet time and a cultural misunderstanding of introverts' needs. Introvert voices are frequently silenced or ignored in group settings such as the workplace, friend groups, basketball teams, bookclubs -- anywhere there's an extrovert speaking, there's an introvert thinking. This app is meant to point the arrow away from telling introverts to change their nature, and instead asking extroverts to think about how their actions are affecting their introverted friends. If you like this app, <a href='http://halented.com' style={{color: 'lightblue'}}>hire me!</a>
                 </div>
             :
-                <div>
+                <div style={thisStyle.block}>
                     <img src={me} className='infoIcon' alt='app creator, hal dunn'/>
                     <span style={{fontWeight: 'bold', fontSize: 'larger'}}>Hal</span> (she/they) is a proud introvert and developer who is seeking a workplace they can call home. They are a driven individual, loving dog parent, devoted partner, and currently in training to compete at the world's strongest man competition in 2021. They are able to complete one pushup so far. Luckily, they are much more adept at programming than lifting. If you are interested in hiring her, please send a message to <span style={{fontWeight: 'bold'}}>heyHal@halented.com</span> or message them on <a href='https://linkedin.com/in/halented' style={{color: 'lightblue'}}>LinkedIn</a>.
                 </div>
