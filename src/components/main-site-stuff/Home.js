0c3d682bab2865552ad28d5d49e31b79c657b324
Hal Dunn <hallie.dunn1@gmail.com>
2020-01-09 16:17:19 -0800

    added radium, testing styles

--- src/components/main-site-stuff/Home.js
+++ src/components/main-site-stuff/Home.js
@@ -1,10 +1,26 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Radium from 'radium';
 
+var styles = {
+    base: {
+        backgroundColor: 'rgb(235, 214, 214)',
+        color: '#282c34',
+        fontFamily: "'Open Sans Condensed', sans-serif",
+        fontSize: 'medium',
+        borderRadius: '3px',
+        boxShadow: '5px 5px #1b1e24',
+        margin: '10%'
+    }
+}
 
-export default class PageCard extends Component {
+class PageCard extends Component {
     render(){
         return (
-            <h1>just some shit you know</h1>
+            <button style={styles.base}>
+                f o r t i f y
+            </button>
         )
     }
 }
+
+export default Radium(PageCard)
--- src/components/pre-site-stuff/Navbar.js
+++ src/components/pre-site-stuff/Navbar.js
@@ -1,15 +1,96 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import Radium from 'radium';
 
-export default function Navbar(){
+const Wrapper = Radium(Link)
 
-    return (
-     <div className='navmenu'>
-        <NavLink to='/home'>Home</NavLink>
-        <NavLink to='/home'>Introverts</NavLink>
-        <NavLink to='/home'>Hangouts</NavLink>
-        <NavLink to='/home'>Metrics</NavLink>
-        <NavLink to='/home'>Settings</NavLink>
-     </div> 
-    )
+
+var styles = {
+    base: {
+        border: '1px solid black',
+        padding: '5px',
+        color: 'lightblue',
+        MozBoxShadow: '0 0 10px #000000',
+        'WebkitBoxShadow': '0 0 10px #000000',
+        boxShadow: '0 0 10px #000000',
+        display: 'flex',
+        flexDirection: 'column',
+    },
+    otra: {
+        ':hover': {
+        backgroundColor: 'red'
+        },
+    
+        ':focus': {
+        backgroundColor: 'green'
+        },
+    
+        ':active': {
+        backgroundColor: 'yellow'
+        }
+    }
 }
+// rgb(154, 218, 240)
+
+class Navbar extends React.Component{
+
+    render(){
+        return (
+         <div key='1' style={styles.base}>
+            <Wrapper to='home' key='2' style={{':hover': {background: 'black'}}}>
+                Home
+            </Wrapper>
+            <Link to='/home' >Introverts</Link>
+            <Link to='/home' >Hangouts</Link>
+            <Link to='/home' >Metrics</Link>
+            <Link to='/home' >Settings</Link>
+         </div> 
+        )
+    }
+
+}
+
+export default Radium(Navbar)
+
+// var styles = {
+//     base: {
+//       background: 'blue',
+//       border: 0,
+//       borderRadius: 4,
+//       color: 'white',
+//       padding: '1.5em',
+  
+//       ':hover': {
+//         backgroundColor: 'red'
+//       },
+  
+//       ':focus': {
+//         backgroundColor: 'green'
+//       },
+  
+//       ':active': {
+//         backgroundColor: 'yellow'
+//       },
+//     },
+  
+//     block: {
+//       display: 'block',
+  
+//       ':hover': {
+//         boxShadow: '0 3px 0 rgba(0,0,0,0.2)'
+//       }
+//     },
+//   };
+  
+//   class PageCard extends React.Component {
+//     render() {
+//         return (
+//         <div key="1" style={styles.base}>
+//             words in first div
+//             <div key="2" style={styles.block}>words in second div</div>
+//         </div>
+//         );
+//     }
+//   }
+  
+//   export default Radium(PageCard);
