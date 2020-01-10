117c7fe816d92a24453fe2f794862110102c9f02
Hal Dunn <hallie.dunn1@gmail.com>
2020-01-10 11:37:24 -0800

    beginning style for homepage

--- src/components/main-site-stuff/Home.js
+++ src/components/main-site-stuff/Home.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
 import Radium from 'radium';
-import { styles } from '../../Styles'
+// import { styles } from '../../Styles'
 
 class Home extends Component {
+    state= {
+        topFriends: []
+    }
+
+    componentDidMount(){
+        // fetch to grab top 3 introverts
+    }
+
     render(){
         return (
-            <button style={styles.button}>
-                h o m e
-            </button>
+            <div>
+                <div>Box full of friends</div>
+                <div>form to add a friend</div>
+                <div>form to add a hangout</div>
+            </div>
         )
     }
 }
