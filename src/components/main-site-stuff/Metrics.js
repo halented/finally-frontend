5920348cfebe98f146833cc453fcc3c4622ae22c
Hallie Dunn <hallie.dunn1@gmail.com>
2020-05-14 16:42:44 -0700

    victory chart getting real, correct data from backend

--- src/components/main-site-stuff/Metrics.js
+++ src/components/main-site-stuff/Metrics.js
@@ -1,13 +1,27 @@
 import React, { useState, useEffect } from 'react';
-import { VictoryChart, VictoryTheme, VictoryLine } from 'victory'
+import { VictoryChart, VictoryTheme, VictoryLine, VictoryAxis } from 'victory'
 import { styles } from '../../Styles'
 import { services } from '../../apiServices'
 
 
 const h3Style = Object.assign(styles.shadowed, {width: '75%'})
+const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"]
 
 function Metrics(){
     const [chartData, changeData] = useState([])
+    // right now, the months don't show up in order because the x values aren't in numerical order.
 
     function setup(){
         services.fetchChartData()
@@ -18,20 +32,33 @@ function Metrics(){
 
     useEffect(setup, [])
 
+    const xDataMaker = () => {
+        // console.log(chartData.map(item => {`${months[item['x']-1]}`}))
+        console.log(chartData)
+        debugger;
+    }
+
         return (
             <div style={styles.columnFlexbox}>
-                <h3 style={h3Style}>Introvert Interaction Metrics</h3>
-                    <VictoryChart theme={VictoryTheme.material}>
-                            <VictoryLine 
-                                style={{
-                                    data: {stroke: "rgb(235, 214, 214)"}, 
-                                    parent: { border: "1px solid #ccc"}
-                                }} 
-                                data={chartData} 
-                            />
+                <h3 style={h3Style}>Hangouts per Month</h3>
+                    <VictoryChart theme={VictoryTheme.material} >
+                        <VictoryLine 
+                            style={{
+                                data: {stroke: "rgb(235, 214, 214)"}, 
+                                parent: { border: "1px solid #ccc"}
+                            }} 
+                            data={chartData}
+                            x={(item)=>`${months[item['x']-1]}`}
+                        />
                     </VictoryChart>
             </div>
         )
 }
 
 export default Metrics
+
+
+
+// (item)=>`${months[item['x']-1]}`
+
+// data.map(item =>`${months[item['x']-1]}`)
