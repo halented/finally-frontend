4d6f0c928bfbeef469030368625ca27b07df93f3
Hallie Dunn <hallie.dunn1@gmail.com>
2020-05-28 11:19:34 -0700

    can now select introvert image

--- src/components/main-site-stuff/Form.js
+++ src/components/main-site-stuff/Form.js
@@ -3,18 +3,29 @@ import Radium from 'radium';
 import { styles } from '../../Styles'
 
 const formBoxStyle = Object.assign({}, styles.forms, styles.shadowed, styles.columnFlexbox)
+const imgArray = ["bear", "bull", "bunny", "dog", "flamingo", "koala", "octopus"]
 
 class Form extends Component {
     state= {
         introvertValue: '',
-        purposeValue: ''
+        purposeValue: '',
+        imageValue: ''
     }
 
     handleChange = (ev, kind) => {
-        if(kind === "introvertValue"){
-            this.setState({introvertValue: ev.target.value})
+        switch(kind){
+            case "introvertValue":
+                this.setState({introvertValue: ev.target.value})
+                break
+            case "purposeValue":
+                this.setState({purposeValue: ev.target.value})
+                break
+            case "imageValue":
+                this.setState({imageValue: ev.target.value})
+                break
+            default:
+                alert("Something's gone wrong. Please try again.")
         }
-        else this.setState({purposeValue: ev.target.value})
     }
 
     render(){
@@ -26,6 +37,12 @@ class Form extends Component {
                     <input placeholder='name' name='name'></input>
                     <label>Recharge Activity:</label>
                     <input placeholder='recharge activity' name='activity'></input>
+                    <label>Image:</label>
+                    <select id='image'value={this.state.imageValue} onChange={(ev)=>this.handleChange(ev, "imageValue")}>
+                        {imgArray.map(image=>{
+                        return <option value={image} key={image}>{image}</option>
+                        })}
+                    </select>
                     <button type='submit'>Submit</button>
                     <span onClick={ () => {this.props.changeShow("")}} style={styles.infoLinks}>Go Back</span>
                 </form>
@@ -39,7 +56,7 @@ class Form extends Component {
                         })}
                     </select>
                     <label>Purpose:</label>
-                    <select id='purpose'value={this.state.purposeValue} onChange={this.handleChange}>
+                    <select id='purpose'value={this.state.purposeValue} onChange={(ev)=>this.handleChange(ev, "purposeValue")}>
                         {this.props.purposes.map(purpose=>{
                         return <option value={purpose.title} key={purpose.title}>{purpose.title}</option>
                         })}
--- src/components/main-site-stuff/IntrovertLink.js
+++ src/components/main-site-stuff/IntrovertLink.js
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom'
 import { styles } from '../../Styles'
 import bear from '../../images/bear.png'
 import bull from '../../images/bull.png'
-import bun from '../../images/bun.png'
+import bunny from '../../images/bun.png'
 import dog from '../../images/dog.png'
-import flam from '../../images/flam.png'
+import flamingo from '../../images/flam.png'
 import koala from '../../images/koala.png'
-import fallback from '../../images/octi.png'
-// test interpolating string from backend into local URL
+import octopus from '../../images/octi.png'
+
 const lowOpacityFriend = Object.assign({}, styles.friendCircle, {opacity: '.4'})
 
 export const IntrovertLink = (props) => {
@@ -18,15 +18,15 @@ export const IntrovertLink = (props) => {
             return  <Link to={`/introverts/${props.int.id}`}><img src={bear} alt={props.int.name} style={on_cooldown? lowOpacityFriend :styles.friendCircle}/> </Link>
         case 'bull':
             return <Link to={`/introverts/${props.int.id}`}><img src={bull} alt={props.int.name} style={on_cooldown? lowOpacityFriend :styles.friendCircle}/></Link>
-        case 'bun':
-            return <Link to={`/introverts/${props.int.id}`}><img src={bun} alt={props.int.name} style={on_cooldown? lowOpacityFriend :styles.friendCircle}/></Link>
+        case 'bunny':
+            return <Link to={`/introverts/${props.int.id}`}><img src={bunny} alt={props.int.name} style={on_cooldown? lowOpacityFriend :styles.friendCircle}/></Link>
         case 'dog':
             return <Link to={`/introverts/${props.int.id}`}><img src={dog} alt={props.int.name} style={on_cooldown? lowOpacityFriend :styles.friendCircle}/></Link>
-        case 'flam':
-            return <Link to={`/introverts/${props.int.id}`}><img src={flam} alt={props.int.name} style={on_cooldown? lowOpacityFriend :styles.friendCircle}/></Link>
+        case 'flamingo':
+            return <Link to={`/introverts/${props.int.id}`}><img src={flamingo} alt={props.int.name} style={on_cooldown? lowOpacityFriend :styles.friendCircle}/></Link>
         case 'koala':
             return <Link to={`/introverts/${props.int.id}`}><img src={koala} alt={props.int.name} style={on_cooldown? lowOpacityFriend :styles.friendCircle}/></Link>
         default:
-            return <Link to={`/introverts/${props.int.id}`}><img src={fallback} alt={props.int.name} style={on_cooldown? lowOpacityFriend :styles.friendCircle}/></Link>
+            return <Link to={`/introverts/${props.int.id}`}><img src={octopus} alt={props.int.name} style={on_cooldown? lowOpacityFriend :styles.friendCircle}/></Link>
     }
 }
